07b23d2c44845cf6114f5f1d8d14759ec023c817
Aidan Killeen <aidan.killeen@gmail.com>
2026-06-24 14:36:16 +0100

    added another sp

--- create_procedure_versatile_insert.sql
+++ create_procedure_versatile_insert.sql
@@ -11,10 +11,10 @@ GO
 -- =============================================
 CREATE OR ALTER PROCEDURE akVersatileInsert
 (
-    @Id int,
     @Name NVARCHAR(50),
     @Email NVARCHAR(50),
-    @Active tinyint 
+    @Active tinyint, 
+    @Id int = NULL
 )
 AS
 BEGIN
@@ -22,7 +22,37 @@ BEGIN
     -- interfering with SELECT statements.
     SET NOCOUNT ON
 
+    BEGIN TRY
+        IF @Id IS NULL
+        BEGIN
+            INSERT INTO ak_Users
+            (Name, Email, Active)
+            VALUES(@Name, @Email, @Active);
+
+            -- how do I determine the Id that 
+            -- was generated
+            SELECT @Id = @@IDENTITY;
+
+        END
+        ELSE
+        BEGIN
+            PRINT 'do insert with id'
+            SET IDENTITY_INSERT ak_Users ON;
+            INSERT INTO ak_Users
+            (Id, Name, Email, Active)
+            VALUES(@Id, @Name, @Email, @Active);
+            SET IDENTITY_INSERT ak_Users OFF;
+        END
+
+        -- insert some sample transactions for this user
+        INSERT INTO ak_UserPurchases
+        (UserId, PurchaseDate, Amount)
+        VALUES(@Id, GETDATE(), 9.99);
+
+    END TRY
+    BEGIN CATCH
+        PRINT 'Can''t insert a duplicate id.'
+    END CATCH
     -- Insert statements for procedure here
-    PRINT 'is this working?'
 END
 GO
---
+++ test_akVersatileInsert.sql
@@ -0,0 +1,39 @@
+EXEC akVersatileInsert 
+    'Veronica', 
+    'veronica@gmail.com', 
+    1, 
+    1011;
+
+
+EXEC sp_akInsertUser 
+    1000,
+    'Veronica', 
+    'veronica@gmail.com', 
+    1;
+
+
+SELECT TOP 5 
+    * 
+FROM ak_Users
+ORDER BY Id DESC;
+
+SELECT TOP 5 
+    * 
+FROM ak_UsersArchive
+ORDER BY Id DESC
+
+
+SELECT * FROM
+(SELECT TOP 5 
+    * 
+FROM ak_Users
+ORDER BY Id DESC) u
+
+UNION
+SELECT * FROM 
+(
+SELECT TOP 5 
+    * 
+FROM ak_UsersArchive
+ORDER BY Id DESC) u2;
+-- SELECT COUNT(*) FROM Customers;
